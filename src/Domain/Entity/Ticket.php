7d8214f04619439366bd634cb3b7c4a785c8a205
Yannick08-cmd <yannick.ranarisoa@gmail.com>
2026-02-22 00:10:57 +0100

    Update Ticket.php
    ajout fonctions, attributs

--- src/Domain/Entity/Ticket.php
+++ src/Domain/Entity/Ticket.php
@@ -3,5 +3,31 @@
 namespace App\Domain\Entity;
 
 class Ticket
-{
+{   public function __construct(
+        private int $id_ticket,
+        public string $title,
+        public string $description,
+        private string $status,
+
+    ){
+        
+    }
+
+    public function saveTicket(): void
+    {
+        // Code to save the ticket to the database
+    }
+
+    public function writeDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+
+
 }
